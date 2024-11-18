de538f461c16065be99a2794ca4b534238d48456
tungnk123 <doanthanhtungnk123@gmail.com>
2024-11-18 22:12:50 +0700

    Init project

---
+++ src/test/java/com/example/demo_stream_processing/DemoStreamProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_stream_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoStreamProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
